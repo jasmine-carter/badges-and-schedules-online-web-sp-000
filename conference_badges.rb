6233c282af9e8c1f324e6b69892d2a75a1d09994
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-22 00:36:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,10 +15,10 @@ badge_messages = []
 end
 
 def assign_rooms(names)
-room_number = 7
-room_list = []
-  names.each do |name|
-    room_list.push["Hello, #{name}! You'll be assigned to room #{room_number}"]
-    room_number = room_number - 1
-end
+  room_number = 7
+  room_list = []
+    names.each do |name|
+      room_list.push["Hello, #{name}! You'll be assigned to room #{room_number}"]
+      room_number = room_number - 1
+  end
 end
